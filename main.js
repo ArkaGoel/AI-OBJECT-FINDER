fc35261387a5527cd91c307c7a241fe7f2c118a4
ArkaGoel <77157313+ArkaGoel@users.noreply.github.com>
2022-08-23 17:56:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let state = false;
 var objects = [];
-var input = "";
+input = document.getElementById("Object");
 
 function preload() {
 
@@ -14,6 +14,7 @@ function modelLoaded() {
     console.log("CoCoSSD Initialized!");
     document.getElementById("status").innerHTML = "Status- Detecting Objects...";
     state = true;
+    objectDetector.detect(video, gotResults);
 }
 
 function setup() {
@@ -23,6 +24,32 @@ function setup() {
     video.hide();
 }
 
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+}
+
 function draw() {
     image(video, 0, 0, 500, 380);
+    if (state != "") {
+        for (i = 0; i < objects.length; i++) {
+            if (objects[i] == input) {
+                r = random(255);
+                g = random(255);
+                b = random(255);
+                fill(r, g, b);
+                noFill();
+                per = floor(objects[i].confidence * 100);
+                stroke(r, g, b);
+                text(objects[i].label + " " + per + "%", objects[i].x + 15, objects[i].y + 15);
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            } else {
+                document.getElementById("Object").innerHTML = "Please enter a value";
+            }
+        }
+    }
 }
